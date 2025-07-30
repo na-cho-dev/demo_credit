6b69c7e549bb3f1097fa7f323fa314c3298deb6e
Fortune Iheanacho <nachodev369@gmail.com>
2025-07-31 00:06:07 +0100

    refactor: use NODE_ENV import for environment variable in database configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,10 @@
 import knex from "knex";
 import logger from "../utils/logger";
+import { NODE_ENV } from "./env";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require("../knexfile");
 
-const env = process.env.NODE_ENV || "development";
+const env = NODE_ENV || "development";
 const db = knex(config[env]);
 
 // Test DB connection
