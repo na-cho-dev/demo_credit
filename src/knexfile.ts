1026f5373afb7aa8f042396ae8d36ce8624d0ec2
Fortune Iheanacho <nachodev369@gmail.com>
2025-07-31 01:20:02 +0100

    feat: add staging and test configurations to knexfile for improved environment support

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -16,6 +16,36 @@ const config = {
       extension: "ts",
     },
   },
+  staging: {
+    client: "mysql2",
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      port: DB_PORT,
+    },
+    migrations: {
+      directory: "./database/migrations",
+      tableName: "knex_migrations",
+      extension: "js",
+    },
+  },
+  test: {
+    client: "mysql2",
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      port: DB_PORT,
+    },
+    migrations: {
+      directory: "./database/migrations",
+      tableName: "knex_migrations",
+      extension: "ts",
+    },
+  },
   production: {
     client: "mysql2",
     connection: {
